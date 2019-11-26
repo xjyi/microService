58aa21172073a43e1ea1f20fa82830660cb73166
xianjinyi <xianjinyi@hzed.com>
2019-11-26 21:16:10 +0800

    算法

--- gameProvider/src/main/java/com/xianjinyi/gameProvider/leetcodeclasswork/ArrayAlgorithm.java
+++ gameProvider/src/main/java/com/xianjinyi/gameProvider/leetcodeclasswork/ArrayAlgorithm.java
@@ -217,6 +217,8 @@ public class ArrayAlgorithm {
 
     /**
      * 求众数
+     *
+     * 还有投票法，见leetcode
      */
     public int majorityElement(int[] nums) {
         if (nums.length == 1){
@@ -237,4 +239,141 @@ public class ArrayAlgorithm {
         }
         return -1;
     }
+
+    /**
+     * 缺失的第一个正数
+     *
+     * 时间复杂度应为O(n)，并且只能使用常数级别的空间 O(1)
+     *
+     *
+     * 负数，大于n的，重复的，这些数值没有意义，所以不换位置，其他的换到正确位置，每个值都处理完后，第一个没有在正确位置的，就是解
+     *
+     * 原理跟使用负数的差不多，这种更容易想到。且不用处理1，这种特殊情况
+     * @param nums
+     * @return
+     */
+    public int firstMissingPositive(int[] nums) {
+        // n个数据，最极端的情况，也是n个连续的正数，否则就有空洞
+        // 最小缺失正数必定 <= n+1
+
+
+        int n = nums.length;
+        // 基于相同的思想，不额外增加空间，那就交换位置
+        for (int i = 0; i < n; i++) {
+            // 处理当前i位置的元素,换完之后，继续判断当前i位置是否需要处理
+            // nums[i]!=i+1  不行，因为i+1这个数可能并不存在，
+            // 这个条件只能证明当前的位置不对，但是也不足以 作为交换位置的根据，只能根据值找他应该存在的位置（该位置如果已经有一个跟当前一样的数据，那就不需交换了）
+            // 数组数据不重复的情况下也是没问题的，但是数据有重复，则会造成死循环
+            while(nums[i] >0 && nums[i]<=n && nums[nums[i] - 1]!= nums[i]){
+                swap(nums,i,nums[i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] !=i+1){
+                return i+1;
+            }
+        }
+
+
+
+
+
+        // 以下是O(n)的解法，不符合要求
+        // 1~n的存储
+//        boolean[] status = new boolean[n];
+//        for (int i = 0; i < n; i++) {
+//            if (nums[i] >0 && nums[i] <=n){
+//                status[nums[i]-1] = true;
+//            }
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            if (!status[i]){
+//                return i+1;
+//            }
+//        }
+        return n+1;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+//        int temp = nums[i];
+//        nums[i] = nums[num];
+//        nums[num] = temp;
+
+
+        nums[index1] = nums[index1] ^ nums[index2];
+        nums[index2] = nums[index1] ^ nums[index2];
+        nums[index1] = nums[index1] ^ nums[index2];
+
+        // 一般就用一个temp即可
+//        如果 a ^ b = c ，那么 a ^ c = b 与 b ^ c = a 同时成立，利用这一条，可以用于交换两个变量的值。
+        // 相同的为0，不同的为1
+
+//        对于异或运算实现的交换方法，如果调用 swap(nums, i, i)，那么最终的结果会变为 0。
+//        对于加减法实现的交换方法，有可能发生溢出。
+    }
+
+
+    /**
+     * 同样是 0，负数，大于n的数转为为1（先确保1已经存在，不是解）
+     * 此时数组中都是正整数，且都在1~n范围内
+     * 则遍历数组，把数值对应的下标置为负数（表示该数已经存在，但是又不影响使用该数据的使用（负数用的时候取绝对值））多出来的0，n刚好互补
+     * 数组中某个元素为正数，说明没有值对应这个下标，就是解
+     * @param nums
+     * @return
+     */
+    public int firstMissingPositive1(int[] nums) {
+        int n = nums.length;
+
+        // 基本情况
+        int contains = 0;
+        for (int i = 0; i < n; i++)
+            if (nums[i] == 1) {
+                contains++;
+                break;
+            }
+
+        if (contains == 0)
+            return 1;
+
+        // nums = [1]
+        if (n == 1)
+            return 2;
+
+        // 用 1 替换负数，0，
+        // 和大于 n 的数
+        // 在转换以后，nums 只会包含
+        // 正数
+        for (int i = 0; i < n; i++)
+            if ((nums[i] <= 0) || (nums[i] > n))
+                nums[i] = 1;
+
+        // 使用索引和数字符号作为检查器
+        // 例如，如果 nums[1] 是负数表示在数组中出现了数字 `1`
+        // 如果 nums[2] 是正数 表示数字 2 没有出现
+        for (int i = 0; i < n; i++) {
+            int a = Math.abs(nums[i]);
+            // 如果发现了一个数字 a - 改变第 a 个元素的符号
+            // 注意重复元素只需操作一次
+            if (a == n)
+                nums[0] = - Math.abs(nums[0]);
+            else
+                nums[a] = - Math.abs(nums[a]);
+        }
+
+        // 现在第一个正数的下标
+        // 就是第一个缺失的数
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0)
+                return i;
+        }
+
+        if (nums[0] > 0)
+            return n;
+
+        return n + 1;
+    }
+
+
 }
---
+++ gameProvider/src/main/java/com/xianjinyi/gameProvider/leetcodeclasswork/LinkedAl.java
@@ -0,0 +1,211 @@
+package com.xianjinyi.gameProvider.leetcodeclasswork;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+/**
+ * @author: xianjinyi
+ * @date 2019/11/25
+ */
+public class LinkedAl {
+
+    // 应该使用优先级队列，但是此处ListNode没有实现comparable 接口
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        ListNode sentinal = new ListNode(-1);
+        ListNode next = sentinal;
+
+        PriorityQueue<ListNode> listNodes = new PriorityQueue<>();
+        for (int i=0;i<lists.length;i++){
+            listNodes.add(lists[i]);
+        }
+
+        while (!listNodes.isEmpty()){
+            ListNode poll = listNodes.poll();
+            next.next = poll;
+            poll = poll.next;
+            if (poll!=null){
+                listNodes.add(poll);
+            }
+        }
+         return sentinal.next;
+
+//
+//
+//
+//        while(true){
+//            int min  = Integer.MAX_VALUE;
+//            int j = -1;
+//            for (int i=0;i<lists.length;i++){
+//                if(lists[i] !=null && lists[i].val <min){
+//                    min = lists[i].val;
+//                    j=i;
+//                }
+//            }
+//            if (j == -1){
+//                break;
+//            }
+//            sentinal.next = lists[j];
+//            lists[j] = lists[j].next;
+////            if (lists[j].next !=null){
+//////
+//////            }
+//        }
+
+
+
+       // return sentinal.next;
+    }
+
+       class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+    public static void main(String[] args) {
+        new LinkedAl().isValid("([)]");
+    }
+
+    char a1 = '(';
+    char a2 = ')';
+    char b1 = '{';
+    char b2 = '}';
+    char c1 = '[';
+    char c2 = ']';
+
+    /**
+     * 需要使用栈，不是队列，所以后插入的要在前面
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+
+
+        LinkedList<Character> queue =  new LinkedList();
+
+        char[] chars = s.toCharArray();
+        for (int i=0;i<chars.length;i++){
+            if (chars[i] == a1 || chars[i] == b1  || chars[i] == c1){
+                queue.addFirst(chars[i]);
+            }else {
+                Character poll = queue.poll();
+                if (poll ==null){
+                    return false;
+                }
+                boolean check = check(poll, chars[i]);
+                if (!check){
+                    return false;
+                }
+            }
+        }
+        if (queue.size()>0){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean check(char poll,char thisChar) {
+        if ( (thisChar == a2 && poll==a1) || (thisChar == b2 && poll==b1) || (thisChar == c2 && poll==c1)){
+           return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+     * @param s
+     * @return
+     * 动态规划方式
+     */
+    public int longestValidParentheses1(String s) {
+            int maxans = 0;
+            int dp[] = new int[s.length()];
+
+            for (int i = 1; i < s.length(); i++) {
+                // 只有 ) 字符的才有可能是有效结束字符
+                if (s.charAt(i) == ')') {
+                    // 前一个是(
+                    if (s.charAt(i - 1) == '(') {
+                        dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+
+                    } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                        dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                    }
+                    maxans = Math.max(maxans, dp[i]);
+                }
+            }
+            return maxans;
+
+    }
+
+
+
+
+
+
+
+
+
+    /**
+     * 动态规划方式
+     * @param s
+     * @return
+     */
+    public int mylongestValidParentheses1(String s) {
+        int max = 0 ;
+        // 默认0
+        int [] status = new int[s.length()];
+        for (int i=1;i<s.length();i++){
+            // 只有结尾是)的才有可能是最长子串的尾部
+            if ( s.charAt(i) == ')'){
+
+                if(s.charAt(i-1) == '('){
+                    // 可以只判断i - 2 >= 0，省略：s.charAt(i-2) == ')'   因为不是）的肯定是0
+                    if (i - 2 >= 0 && s.charAt(i-2) == ')'){
+                        status[i] = status[i - 2] + 2 ;
+                    }else{
+                        status[i] = 2;
+                    }
+
+
+                    // status[i-1] > 0 && s.charAt(i-1) == ')'  可省略
+                    // 首先status[i-1] > 0 就包含了 s.charAt(i-1) == ')'
+                    // 如果status[i-1] == 0，s.charAt(i - status[i-1] -1) 的条件跟上一个if相同，不可能成立，所以可以省略
+                    // 以上只是优化
+                }else if (status[i-1] > 0 && s.charAt(i-1) == ')' ){
+
+                    if (i - status[i-1] -1 >=0 && s.charAt(i - status[i-1] -1) == '('){
+                        status[i] = i - status[i-1] -2 >=0?status[i-1] + status[i - status[i-1] -2] +2:status[i-1]  +2;
+                    }
+                    // && i - status[i-1] -1 >0 && status[i - status[i-1] -1] =='('
+
+                }
+            }
+
+            max = Math.max(max, status[i]);
+        }
+
+        // 求最大值的方法，可以直接在循环中进行判断替换
+//        for (int i=0;i<s.length();i++){
+//            if (status[i] >max){
+//                max = status[i];
+//            }
+//        }
+        return max;
+    }
+
+
+    /**
+     * 栈方式
+     * @param s
+     * @return
+     */
+    public int mylongestValidParentheses2(String s) {
+        int max=0;
+
+        return max;
+    }
+}
