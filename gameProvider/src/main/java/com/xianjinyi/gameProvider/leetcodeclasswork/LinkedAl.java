0f26b8e925f73eacd255a43b8b9a378bf3809259
xianjinyi <xianjinyi@hzed.com>
2019-11-30 16:51:19 +0800

    算法

--- gameProvider/src/main/java/com/xianjinyi/gameProvider/leetcodeclasswork/LinkedAl.java
+++ gameProvider/src/main/java/com/xianjinyi/gameProvider/leetcodeclasswork/LinkedAl.java
@@ -1,8 +1,6 @@
 package com.xianjinyi.gameProvider.leetcodeclasswork;
 
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @author: xianjinyi
@@ -207,5 +205,269 @@ public class LinkedAl {
         int max=0;
 
         return max;
+    }
+
+
+    /**
+     * 不需额外空间
+     * @param s
+     * @return
+     */
+    public int mylongestValidParentheses3(String s) {
+        // 从左往右 左括号多不要紧，直到数量等于右括号，则当前长度=有括号*2；
+        int max=0;
+        int left = 0,right =0;
+        for (int i=0;i<s.length();i++){
+            if (s.charAt(i) == '('){
+                left++;
+            }else {
+                right++;
+            }
+
+            if (left == right){
+                max = Math.max(max,right*2);
+            }else if(right > left){
+                left = right =0;
+            }
+        }
+
+        left = right =0;
+
+        for (int i=s.length()-1;i>=0;i--){
+            if (s.charAt(i) == ')'){
+                right++;
+            }else {
+                left++;
+            }
+
+            if (left == right){
+                max = Math.max(max,left*2);
+            }else if(left > right){
+                left = right =0;
+            }
+        }
+
+        return max;
+    }
+
+    /**
+     * 滑动窗口最大值
+     * 要求时间复杂度 O(n)
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] myMaxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        // 以k为大小，间隔成n/k(+1) 个区间，小区间内最右是最大数据
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        left[0] = nums[0];
+        right[n-1] = nums[n-1];
+
+        for (int i = 1; i < n; i++) {
+            if (i % k == 0) {
+                left[i] = nums[i];
+            }else{
+                left[i]= Math.max(nums[i],left[i-1]);
+            }
+
+            // 最右一个是n-1，与i建立关系就是 n-1 -i
+            int j= n-i-1;
+            if ((j+1)%k == 0){
+                right[j] = nums[j];
+            }else{
+                right[j] = Math.max(nums[j],right[j+1]);
+            }
+
+        }
+
+        int[] ints = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++) {
+            ints[i] = Math.max(left[i + k - 1], right[i]);
+        }
+        return ints;
+
+
+        //理解错误，是要每个窗口中的最大值
+//        int[] ints = new int[k];
+//        int[] sums = new int[nums.length - k + 1];
+//        int sum = 0;
+//
+//        for (int i = 0; i < k; i++) {
+//            sum += nums[k];
+//        }
+//        sums[0] = sum;
+//        int max = sums[0];
+//        int maxIndex = 0;
+//
+//        for (int i = k; i < nums.length; i++) {
+//            sums[i-k +1] = sums[i-k] + nums[i] - nums[i-k];
+//            if (max < sums[i-k +1]){
+//                max = sums[i-k +1];
+//                maxIndex = i-k +1;
+//            }
+//
+//        }
+//
+//        for (int j=0;j<k;j++,maxIndex++){
+//            ints[j] = nums[maxIndex];
+//        }
+//        int[] ints = new int[k];
+        // 下一个组合，总大小sum[i] 与 sum[i-1]  相同，则最大值相同
+        // sum[i-1] 更大 ，说明新的值不及原来的区间最左元素大，此时旧区间最大值假如不是最左元素，则最大值不变
+        //  是最左元素，取剩余的最大元素
+        // sum[i] 更大，新的值比原来区间最左大，如果此元素比原最大元素大，则为新元素，否则不变
+
+
+
+
+//        return ints;
+    }
+
+
+    /**
+     * 双向队列方式
+     */
+    class Solution {
+        // 存储的是下标
+        ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
+        int [] nums;
+
+        public void clean_deque(int i, int k) {
+            // 首元素不是当前滑动区间的
+            if (!deq.isEmpty() && deq.getFirst() == i - k){
+                deq.removeFirst();
+            }
+
+
+            // 从后往前，比当前小的都删掉，这些都不可能是当前区间最大的，且对后面的没有作用
+            while (!deq.isEmpty() && nums[i] > nums[deq.getLast()]) {
+                deq.removeLast();
+            }
+        }
+
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            int n = nums.length;
+            if (n * k == 0) {
+                return new int[0];
+            }
+            if (k == 1) {
+                return nums;
+            }
+
+            // init deque and output
+            this.nums = nums;
+            //
+//            int max_idx = 0;
+            for (int i = 0; i < k; i++) {
+                clean_deque(i, k);
+                deq.addLast(i);
+                // compute max in nums[:k]
+//                if (nums[i] > nums[max_idx]){
+//                    max_idx = i;
+//                }
+            }
+
+            int [] output = new int[n - k + 1];
+            // 当前队列最前面的肯定是最大的
+            output[0] = nums[deq.getFirst()];
+
+            // build output
+            for (int i  = k; i < n; i++) {
+                clean_deque(i, k);
+                deq.addLast(i);
+                output[i - k + 1] = nums[deq.getFirst()];
+            }
+            return output;
+        }
+    }
+
+
+
+
+
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int n = nums.length;
+        // 数据为空等
+        if (n * k == 0) {
+            return new int[0];
+        }
+        if (k == 1) return nums;
+
+        // 起始到当前下标，k个数据内的最大值
+        int [] left = new int[n];
+        left[0] = nums[0];
+
+        // 同理，方向从右往左
+        int [] right = new int[n];
+        right[n - 1] = nums[n - 1];
+
+
+
+        for (int i = 1; i < n; i++) {
+            // 则：小区间的最右，是该区间的最大
+            // 按照k个大小的区间划分，区间的起点下标，先直接等于原数组元素
+            if (i % k == 0) {
+                left[i] = nums[i];  // block_start
+            }else {
+                // 其他的等于当前与上一个的最大值
+                left[i] = Math.max(left[i - 1], nums[i]);
+            }
+
+            // 同理 小区间的最左，是该区间的最大
+            int j = n - i - 1;
+            if ((j + 1) % k == 0) {
+                right[j] = nums[j];  // block_end
+            }else {
+                right[j] = Math.max(right[j + 1], nums[j]);
+            }
+        }
+
+        int [] output = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++){
+            output[i] = Math.max(left[i + k - 1], right[i]);
+        }
+
+
+        return output;
+    }
+
+    /**
+     * 爬楼梯
+     *
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     *
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+
+
+
+        return -1;// toClient(n);
+
+    }
+
+    public static HashMap<Integer,Integer> map =  new HashMap<>();
+    private int toClient(int n) {
+        // n递增，用数组即可
+        if (n == 1){
+            return 1;
+        }
+        if (n == 2){
+            return 2;
+        }
+        if (map.containsKey(n)){
+            return map.get(n);
+        }else{
+            int i = toClient(n - 1) + toClient(n - 2);
+            map.put(n,i);
+            return i;
+        }
+
+
     }
 }
