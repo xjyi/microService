ac2a263f1de45c762f2aa923f1647f211a414419
xianjinyi <xianjinyi@hzed.com>
2020-03-28 21:25:36 +0800

    git

---
+++ api-gateway/src/main/java/com/xianjinyi/config/ApiGateWayApplication.java
@@ -0,0 +1,17 @@
+package com.xianjinyi.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayApplication.class, args);
+	}
+
+}
